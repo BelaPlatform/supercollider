00ea9a8be9deb80a5abccacf77af5b5093515645
Giulio Moro <giuliomoro@yahoo.it>
2018-09-08 19:26:27 +0100

    Added support for Bela
    Co-authored-by: elgiano <elgiano@gmail.com>
    Co-authored-by: sensestage <nescivi@gmail.com>
    Co-authored-by: Dan Stowell <danstowell@users.sourceforge.net>
    Co-authored-by: Andrew McPherson <a.mcpherson@qmul.ac.uk>

--- include/plugin_interface/SC_World.h
+++ include/plugin_interface/SC_World.h
@@ -21,6 +21,11 @@
 
 #pragma once
 
+#ifdef SC_BELA
+#    include "Bela.h"
+#    include "SC_BelaScope.h"
+#endif // SC_BELA
+
 #include "SC_Types.h"
 #include "SC_Rate.h"
 #include "SC_SndBuf.h"
@@ -103,6 +108,23 @@ struct World {
     nova::padded_rw_spinlock* mAudioBusLocks;
     nova::spin_lock* mControlBusLock;
 #endif
+
+#ifdef SC_BELA
+    BelaContext* mBelaContext;
+    BelaScope* mBelaScope;
+    uint32 mBelaMaxScopeChannels;
+    uint32 mBelaAnalogInputChannels;
+    uint32 mBelaAnalogOutputChannels;
+    uint32 mBelaDigitalChannels;
+    float mBelaHeadphoneLevel;
+    float mBelaPGAGainLeft;
+    float mBelaPGAGainRight;
+    bool mBelaSpeakerMuted;
+    float mBelaDACLevel;
+    float mBelaADCLevel;
+    uint32 mBelaNumMuxChannels;
+    uint32 mBelaPRU;
+#endif // SC_BELA
 };
 
 inline SndBuf* World_GetBuf(struct World* inWorld, uint32 index) {
