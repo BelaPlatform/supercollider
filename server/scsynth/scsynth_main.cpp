70e7e3c78a063ea63e8c96b873bd62ac9435befa
Brian Heim <self@brianlheim.com>
2021-02-02 16:16:45 -0600

    scsynth_main.cpp: make initializer static

--- server/scsynth/scsynth_main.cpp
+++ server/scsynth/scsynth_main.cpp
@@ -37,9 +37,10 @@
 #    include <unistd.h> // for _POSIX_MEMLOCK
 #    include <sys/wait.h>
 #endif
+
 #ifdef __COBALT__
 #    include "XenomaiLock.h"
-XenomaiInitializer xenomaiInitializer;
+static XenomaiInitializer xenomaiInitializer;
 #endif // __COBALT__
 
 #ifdef _WIN32
